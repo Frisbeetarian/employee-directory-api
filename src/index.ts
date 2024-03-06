f848904c26c83b75a5cca58849e432d2b7220b3d
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-06 16:07:50 +0200

    add test entities and setup connection to db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
+import { Employee } from './entities/Employee';
+import helmet from 'helmet';
 
 const app = express();
 const port = process.env.PORT || 4020;
 
-app.get('/', (req, res) => {
-    res.send('hello');
+// Middleware
+app.use(helmet());
+
+// Database connection
+createConnection({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Employee],
+    synchronize: true,
+    logging: false,
+})
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((error) => {
+        console.error('Database connection error:', error);
+        process.exit(1);
+    });
+
+
+app.use((err: Error, _: express.Request, res: express.Response, __: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
 });
 
 app.listen(port, () => {
