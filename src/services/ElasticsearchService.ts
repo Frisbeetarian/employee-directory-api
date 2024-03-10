e2f7fcaeb1197475fa9c4ee44d9cc071871c1566
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-10 12:46:10 +0200

    add elastisearch service and stub out functionality to search and fitler by department

---
+++ src/services/ElasticsearchService.ts
@@ -0,0 +1,32 @@
+import { Client } from '@elastic/elasticsearch';
+
+class ElasticsearchService {
+    private client: Client;
+
+    constructor() {
+        this.client = new Client({ node: 'http://localhost:9200' });
+    }
+
+    async searchEmployees(query: string) {
+        const result = await this.client.search({
+            index: 'employees',
+            body: {
+                query: {
+                    multi_match: {
+                        query: query,
+                        fields: [
+                            'name',
+                            'jobTitle',
+                            'department',
+                            'location'
+                        ],
+                    },
+                },
+            },
+        });
+        return result.body.hits.hits.map((hit) => hit._source);
+    }
+
+    async filterEmployeesByDepartment(department: string) {
+    }
+}
